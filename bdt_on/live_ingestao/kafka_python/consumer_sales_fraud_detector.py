db2293feb2985525d69f98b21e1ef8b41262dd1f
Thiago Nogueira <tnascimn@everis.com>
2021-05-05 00:28:18 -0300

    subscreve no tópico para corrdenação do grupo

--- bdt_on/live_ingestao/kafka_python/consumer_sales_fraud_detector.py
+++ bdt_on/live_ingestao/kafka_python/consumer_sales_fraud_detector.py
@@ -4,7 +4,14 @@ import time
 from kafka import KafkaConsumer
 
 
-consumer = KafkaConsumer('MP_SALES', bootstrap_servers='hdpdemo.local:6667', group_id='fraud_detector')
+consumer = KafkaConsumer(
+    'MP_SALES', 
+    bootstrap_servers='hdpdemo.local:6667', 
+    group_id='fraud_detector', 
+    max_poll_interval_ms=1000, 
+    heartbeat_interval_ms=200, 
+    )
+consumer.subscribe(topics=['MP_SALES'])
 for msg in consumer:
     time.sleep(1)
     if json.loads(msg.value)['after']['PRICE'] > 600:
